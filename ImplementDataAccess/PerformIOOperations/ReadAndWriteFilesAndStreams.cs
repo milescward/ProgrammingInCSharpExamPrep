afb65376c5a203f0d4ce76051c7c4016fe640458
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-07 20:18:41 -0800

    IO use

--- ImplementDataAccess/PerformIOOperations/ReadAndWriteFilesAndStreams.cs
+++ ImplementDataAccess/PerformIOOperations/ReadAndWriteFilesAndStreams.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.IO;
+
 namespace ImplementDataAccess.PerformIOOperations
 {
     public class ReadAndWriteFilesAndStreams
     {
-        public ReadAndWriteFilesAndStreams()
+        /*If you want to add data to an existing file that may already exist,
+         * then you should use an Appendxxx method. If you want to overwrite
+         * an existing file, then you should use a Writexxx method. Then,
+         * depending on how you want the information is stored (whether as
+         * binary data, a textual blob in a string, or an array of strings
+         * representing each individual line) use the xxxAllBytes, xxxAllText,
+         * or xxxAllLines method.*/
+
+        static void Main(string[] args)
         {
+            string path = "/Users/milesward/Projects/ImplementDataAccess/ImplementDataAccess/PerformIOOperations/test.txt";
+            string[] content = { "Hello,", "world" };
+            File.AppendAllLines(path, content);
         }
     }
 }
--- ImplementDataAccess/QueryAndManipulateDataWLINQ/QueryUsingOperators/QueryDataUsingOperators.cs
+++ ImplementDataAccess/QueryAndManipulateDataWLINQ/QueryUsingOperators/QueryDataUsingOperators.cs
@@ -8,118 +8,119 @@ namespace ImplementDataAccess.QueryAndManipulateDataWLINQ
 {
     public class QueryDataUsingOperators
     {
-        static void Main(string[] args)
-        {
-            var cars = ProcessFile("/Users/milesward/Projects/ImplementDataAccess/ImplementDataAccess/QueryAndManipulateDataWLINQ/QueryUsingOperators/fuel.csv");
+
+        //static void Main(string[] args)
+        //{
+        //    var cars = ProcessFile("/Users/milesward/Projects/ImplementDataAccess/ImplementDataAccess/QueryAndManipulateDataWLINQ/QueryUsingOperators/fuel.csv");
 
 
-            //method syntax
-            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            .First();
+        //    //method syntax
+        //    var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+        //                    .OrderByDescending(c => c.Combined)
+        //                    .ThenBy(c => c.Name)
+        //                    .First();
 
-            // The First() operator does not use deferred execution, and the
-            // query executes immediately. The top variable can now be used
-            // as an instance of a car.
+        //    // The First() operator does not use deferred execution, and the
+        //    // query executes immediately. The top variable can now be used
+        //    // as an instance of a car.
 
-            Console.WriteLine(top.Name);
+        //    Console.WriteLine(top.Name);
 
-            //query syntax
-            var query2 =
-                from car in cars
-                where car.Manufacturer == "BMW" && car.Year == 2016
-                orderby car.Combined descending, car.Name ascending
-                select car;
+        //    //query syntax
+        //    var query2 =
+        //        from car in cars
+        //        where car.Manufacturer == "BMW" && car.Year == 2016
+        //        orderby car.Combined descending, car.Name ascending
+        //        select car;
 
-            // ANONYMOUS TYPES
+        //    // ANONYMOUS TYPES
 
-            // Declaring an object of no specific type and assigning
-            // characteristics to it.
+        //    // Declaring an object of no specific type and assigning
+        //    // characteristics to it.
 
-            var result = cars.Select(c => new
-            {
-                c.Manufacturer,
-                c.Name,
-                c.Combined
-            });
+        //    var result = cars.Select(c => new
+        //    {
+        //        c.Manufacturer,
+        //        c.Name,
+        //        c.Combined
+        //    });
 
-            // if you hover over the properties above, they are of an
-            // anonymous type. This means that if you try to send this
-            // data somewhere, you are not potentially transferring
-            // a huge amount of extra data with those objects.
+        //    // if you hover over the properties above, they are of an
+        //    // anonymous type. This means that if you try to send this
+        //    // data somewhere, you are not potentially transferring
+        //    // a huge amount of extra data with those objects.
 
-            Console.WriteLine(result);
+        //    Console.WriteLine(result);
 
-            // Print out the top ten
-            foreach (var car in query2.Take(10))
-            {
-                Console.WriteLine($"{car.Manufacturer}{car.Name} :" +
-                    $" {car.Combined}");
-            }
+        //    // Print out the top ten
+        //    foreach (var car in query2.Take(10))
+        //    {
+        //        Console.WriteLine($"{car.Manufacturer}{car.Name} :" +
+        //            $" {car.Combined}");
+        //    }
 
-            // FORCE EXECUTION OF A QUERY
+        //    // FORCE EXECUTION OF A QUERY
 
-            /* When we use certain LINQ extension methods, in particular the
-             * ones that return an abstract collection, we have deferred
-             * execution available to us.
-             *
-             * Deferred execution is a lazy way that the queries execute,
-             * which means that they don't complete the query until it is
-             * absolutely necessary.
-             *
-             * var query = cars.where(c => c.Name == BMW)
-             *
-             * The preceeding statement isnt executed until another method
-             * calls on the query variable.
-             *
-             * The yield return keywords show that a method offers deferred
-             * execution.
-             *
-             * Force execution happens when the query needs to return a
-             * concrete response, like an actual collection or object.
-             *
-             * var query = cars.where(c => c.Name == BMW).ToList()
-             *
-             * The preceeding query must execute as soon as it is called,
-             * because the ToList() method must return a concrete collection
-             * right away.
-             */
+        //    /* When we use certain LINQ extension methods, in particular the
+        //     * ones that return an abstract collection, we have deferred
+        //     * execution available to us.
+        //     *
+        //     * Deferred execution is a lazy way that the queries execute,
+        //     * which means that they don't complete the query until it is
+        //     * absolutely necessary.
+        //     *
+        //     * var query = cars.where(c => c.Name == BMW)
+        //     *
+        //     * The preceeding statement isnt executed until another method
+        //     * calls on the query variable.
+        //     *
+        //     * The yield return keywords show that a method offers deferred
+        //     * execution.
+        //     *
+        //     * Force execution happens when the query needs to return a
+        //     * concrete response, like an actual collection or object.
+        //     *
+        //     * var query = cars.where(c => c.Name == BMW).ToList()
+        //     *
+        //     * The preceeding query must execute as soon as it is called,
+        //     * because the ToList() method must return a concrete collection
+        //     * right away.
+        //     */
 
 
 
-            // QUANTIFIERS
+        //    // QUANTIFIERS
 
-            // Any()
-            // This just 
+        //    // Any()
+        //    // This just 
 
-            // This just returns a bool that represents whether or not
-            // there are any elements at all in the collection.
-            //      bool result1 = cars.Any();
+        //    // This just returns a bool that represents whether or not
+        //    // there are any elements at all in the collection.
+        //    //      bool result1 = cars.Any();
 
-            // Do any cars have a manufacturer of Ford?
-            // as soon as the Any method finds something that is a ford,
-            // it retuns true.
-            //      bool result2 = cars.Any(c => c.Manufacturer == "Ford");
+        //    // Do any cars have a manufacturer of Ford?
+        //    // as soon as the Any method finds something that is a ford,
+        //    // it retuns true.
+        //    //      bool result2 = cars.Any(c => c.Manufacturer == "Ford");
 
-            // Do all the cars have a manufacturer of Ford?
-            // as soon as the all method finds something that isn't a ford,
-            // it retuns false.
-            //      bool result3 = cars.All(c => c.Manufacturer == "Ford");
+        //    // Do all the cars have a manufacturer of Ford?
+        //    // as soon as the all method finds something that isn't a ford,
+        //    // it retuns false.
+        //    //      bool result3 = cars.All(c => c.Manufacturer == "Ford");
 
-            // PROJECTING DATA
+        //    // PROJECTING DATA
 
-            /* Projection refers to the operation of transforming an object
-             * into a new form that often consists only of those properties
-             * that will be subsequently used. By using projection, you can
-             * construct a new type that is built from each object.
-             *
-             * Extension methods are perfect. See the Car.cs file for the
-             * .ToCar() extension method.  
-             */
+        //    /* Projection refers to the operation of transforming an object
+        //     * into a new form that often consists only of those properties
+        //     * that will be subsequently used. By using projection, you can
+        //     * construct a new type that is built from each object.
+        //     *
+        //     * Extension methods are perfect. See the Car.cs file for the
+        //     * .ToCar() extension method.  
+        //     */
 
-            // Primary projection operator is .Select()
-        }
+        //    // Primary projection operator is .Select()
+        //}
 
         private static List<Car> ProcessFile(string path)
         {
